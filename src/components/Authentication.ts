307122b51d5411d46657dffcb332e4b79c342d84
Connor Winnestaffer <cwinnestaffer@dminc.com>
2021-05-08 20:26:34 -0400

    typing

--- src/components/Authentication.ts
+++ src/components/Authentication.ts
@@ -7,28 +7,30 @@ import { Instructions } from "./Instructions";
 
 export const Authentication = () => {
   const { login } = React.useContext(AuthContext);
-  const initialState = {
-    userName: "",
-    userNameSet: false,
-    authenticated: false,
-    showWarning: false,
-  };
-  const [state, setState] = useState(initialState);
+  // const initialState = {
+  //   userName: "",
+  //   userNameSet: false,
+  //   authenticated: false,
+  //   showWarning: false,
+  // };
+  // const [state, setState] = useState(initialState);
+  const [userName, setUserName] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
 
-  const setUserName = () => {
-    if (!!state.userName && state.userName.length > 3) {
-      setState({ userNameSet: true, showWarning: false });
-      login(state.userName);
-    } else {
-      setState({ userNameSet: false, showWarning: true });
-    }
-  };
+  // const setUserName = () => {
+  //   if (!!state.userName && state.userName.length > 3) {
+  //     setState({ userNameSet: true, showWarning: false });
+  //     login(state.userName);
+  //   } else {
+  //     setState({ userNameSet: false, showWarning: true });
+  //   }
+  // };
 
   const handleChange = (event) => {
-    setState({ userName: event.target.value });
+    setUserName(event.target.value);
   };
 
-  let warning = state.showWarning ? (
+  let warning = showWarning ? (
     <h3>username must be more than 3 characters</h3>
   ) : null;
   let loginView = (
